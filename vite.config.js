d16749beddc38b5a108047dd3a0c7bf7107de648
DanielBrownsky <ayekudaniel@gmail.com>
2025-04-28 16:49:00 +0100

    change the base on package.json

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/siwes-tracker/',
+  base: '/',
   plugins: [react()],
-  
+  
 })
