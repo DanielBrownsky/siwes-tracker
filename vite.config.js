b2eb0c75d40734df83f51f994e8a2f0fdd8cb20e
DanielBrownsky <ayekudaniel@gmail.com>
2025-04-28 11:54:09 +0100

    add pages folder and components

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/siwes-tracker/',
   plugins: [react()],
-  base: "/siwes-tracker/"
+  
 })
